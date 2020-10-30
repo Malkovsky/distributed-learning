31c1fbf63d17d62d1233a5a1ad68e3754bece730
TihonkovSergey <slezki.edinoroga@mail.ru>
2020-10-31 00:12:43 +0300

    Added submodule meliketoy/wide-resnet.pytorch and renamed to wide_resnet_submodule

--- wide_resnet_submodule/networks/lenet.py
+++ wide_resnet_submodule/networks/lenet.py
@@ -1,13 +1,11 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 def conv_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         init.xavier_uniform(m.weight, gain=np.sqrt(2))
         init.constant(m.bias, 0)
-
 
 class LeNet(nn.Module):
     def __init__(self, num_classes):
