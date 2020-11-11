5cbafb59f7ca1ad9536b2384a1fa38921f9ef5c9
TihonkovSergey <slezki.edinoroga@mail.ru>
2020-11-11 04:17:14 +0300

    docs added

--- utils/consensus_node.py
+++ utils/consensus_node.py
@@ -2,13 +2,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from wide_resnet_submodule.networks import *
 
-model_type = {
-    'lenet': LeNet,
-    'vggnet': VGG,
-    'resnet': ResNet,
-    'wide-resnet': Wide_ResNet
-}
-
 
 class ConsensusNode:
     # TODO: поддержка  GPU
@@ -20,6 +13,15 @@ class ConsensusNode:
                  lr=0.02,
                  stat_step=50,
                  verbose=0):
+        """
+        Class implementing consensus node in consensus network.
+        :param name: unique node name in consensus network
+        :param weights: dict of node names and weights corresponding edges
+        :param train_loader: generator of train batches
+        :param lr: gradient learning rate
+        :param stat_step: period of statistic save
+        :param verbose: verbose mode
+        """
         self.model = None
         self.optimizer = None
         self.opt_args = None
@@ -45,42 +47,100 @@ class ConsensusNode:
         self.verbose = verbose
         self.debug_file = sys.stdout
 
-    def save_accuracy(self, accuracy, it):
+    def save_accuracy(self, accuracy, iteration):
+        """
+        Saves model accuracy
+        :param accuracy: float accuracy
+        :param iteration: iteration number
+        :return: self
+        """
         self.accuracy_list[0].append(accuracy)
-        self.accuracy_list[1].append(it)
-        self._print_debug(f"Node {self.name}: iter {it}, accuracy= {accuracy:.2f}", verbose=1)
+        self.accuracy_list[1].append(iteration)
+        self._print_debug(f"Node {self.name}: iter {iteration}, accuracy= {accuracy:.2f}", verbose=1)
+        return self
 
     def save_loss(self, it):
+        """
+        Saves cumulative model loss and zeroing this
+        :param it:
+        :return:
+        """
         self.loss_list[0].append(self.loss_cum)
         self.loss_list[1].append(it)
         self._print_debug(f"Node {self.name}: iter {it}, cumulative train loss= {self.loss_cum:.2f}", verbose=1)
         self.loss_cum = 0.
+        return self
 
     def _print_debug(self, msg, verbose):
+        """
+        Print msg if good verbose mode
+        :param msg: string of message
+        :param verbose: verbose mode
+        :return: self
+        """
         if verbose <= self.verbose:
             print(msg, file=self.debug_file)
+        return self
 
     def set_model(self, model, *args, **kwargs):
+        """
+        Sets self.model on given model
+        :param model: some model with interface like models in pytorch
+        :param args: other unnamed params
+        :param kwargs: other named params
+        :return: self
+        """
         self.model = model(*args, *kwargs)
         self._print_debug(f"Node {self.name} set model={self.model} with args={args}, kwargs={kwargs}", 2)
+        return self
 
     def set_optimizer(self, optimizer, *args, **kwargs):
+        """
+        Sets self optimizer on given optimizer
+        :param optimizer: some pytorch optimizer
+        :param args: other unnamed params
+        :param kwargs: other named params
+        :return: self
+        """
         self.optimizer = optimizer
         self.opt_args = args
         self.opt_kwargs = kwargs
         self._print_debug(f"Node {self.name} set optimizer={self.optimizer} with args={args}, kwargs={kwargs}", 2)
+        return self
 
     def set_error(self, error, *args, **kwargs):
+        """
+        Sets self error function on given func
+        :param error: some error function
+        :param args: other unnamed params
+        :param kwargs: other named params
+        :return: self
+        """
         self.error = error(*args, **kwargs)
         self._print_debug(f"Node {self.name} set error={self.error} with args={args}, kwargs={kwargs}", 2)
+        return self
 
     def set_neighbors(self, neighbors):
+        """
+        Sets node's neighbors on given neighbors
+        :param neighbors: dict of nodes names and links
+        :return:
+        """
         self.neighbors = neighbors
+        return self
 
     def get_params(self):
+        """
+        Returns model parameters
+        :return: model parameters
+        """
         return self.model.parameters()
 
     def show_graphs(self):
+        """
+        Shows all statistics
+        :return: nothing
+        """
         fig, axs = plt.subplots(figsize=(20, 8), ncols=2)
         fig.suptitle(f'{self.name}', fontsize=24)
         fig.tight_layout(pad=4.0)
@@ -92,6 +152,10 @@ class ConsensusNode:
         axs[1].set_ylabel('Loss', fontsize=16)
 
     def ask_params(self):
+        """
+        Asks and saves neighbors model parameters
+        :return: self
+        """
         self.parameters = {node_name: node.get_params()
                            for node_name, node in self.neighbors.items()}
-
+        return self
